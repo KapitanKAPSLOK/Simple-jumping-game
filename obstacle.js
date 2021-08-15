e13fe3cf3d06bacf810c72a86692f0651c6b8b66
KapitanKAPSLOK <KapitanKAPSLOK@users.noreply.github.com>
2021-08-15 20:12:05 +0000

    deploy: 9bd85edba5d39aed97c306f6fa8a7e7bdccfbc84

--- obstacle.js
+++
@@ -1,9 +0,0 @@
-import MovingDiv from './moving_div.js';
-export default class Obstacle extends MovingDiv {
-    constructor(width, height, speedXPxPerSec, speedYPxPerSec) {
-        super(window.innerWidth - width, 0, width, height, speedXPxPerSec, speedYPxPerSec);
-        this.div.style.backgroundColor = 'black';
-        this.speedXPxPerSec = speedXPxPerSec;
-        this.speedYPxPerSec = speedYPxPerSec;
-    }
-}
